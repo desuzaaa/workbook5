7bd6155acfe18a998b7746bc41e7e9f284e2c7ec
Kameron de Suza <z6nekam@gmail.com>
2024-10-29 15:57:23 -0400

    did exercises

---
+++ es6_cheap_candy.js
@@ -0,0 +1,35 @@
+let products = [
+  { product: "Gummy Worms", price: 5.79 },
+  { product: "Plain M&Ms", price: 2.89 },
+  { product: "Peanut M&Ms", price: 2.89 },
+  { product: "Swedish Fish", price: 3.79 },
+  // TO-DO: fill the array with 10 candies of various
+  // price ranges
+];
+
+// Which candies costs less than $4.00?
+// Which candies has "M&M" its name?
+// Do we carry "Swedish Fish"?
+
+let cheapCandy = () => {
+  return products.filter(product => product.price < 4);
+  
+
+};
+console.log(cheapCandy());
+
+
+
+let ownChocolate = () => {
+    return products.filter(product => product.product.includes ("M&M"));
+    
+}
+
+console.log(ownChocolate());
+
+let ownSwedishFish = () => {
+    return products.find(product => product.product == "Swedish Fish")
+}
+
+console.log(ownSwedishFish());
+
